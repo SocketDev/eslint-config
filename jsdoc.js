cf76978d8ef572bb1a191e91706efbb9c53969a6
Pelle Wessman <pelle@socket.dev>
2023-02-13 12:05:38 +0100

    Tweaks to better test on external repos

---
+++ jsdoc.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+  'plugins': [
+    'jsdoc'
+  ],
+  'extends': [
+    './',
+    'plugin:jsdoc/recommended'
+  ],
+  root: true,
+  rules: {
+    "jsdoc/check-types": "off",
+    "jsdoc/no-undefined-types": "off",
+    "jsdoc/require-jsdoc": "warn",
+    "jsdoc/require-param-description": "off",
+    "jsdoc/require-property-description": "off",
+    "jsdoc/require-returns-description": "off",
+    "jsdoc/require-yields": "off",
+    "jsdoc/valid-types": "off"
+  }
+}
