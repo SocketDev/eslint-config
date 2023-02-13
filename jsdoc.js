f556f3092ff14a2af3eab81991eb259674450fc2
Pelle Wessman <pelle@socket.dev>
2023-02-13 12:06:48 +0100

    Add correct settings

--- jsdoc.js
+++ jsdoc.js
@@ -1,22 +1,27 @@
 'use strict'
 
 module.exports = {
-  'plugins': [
+  plugins: [
     'jsdoc'
   ],
-  'extends': [
+  extends: [
     './',
     'plugin:jsdoc/recommended'
   ],
   root: true,
+  settings: {
+    jsdoc: {
+      mode: 'typescript'
+    }
+  },
   rules: {
-    "jsdoc/check-types": "off",
-    "jsdoc/no-undefined-types": "off",
-    "jsdoc/require-jsdoc": "warn",
-    "jsdoc/require-param-description": "off",
-    "jsdoc/require-property-description": "off",
-    "jsdoc/require-returns-description": "off",
-    "jsdoc/require-yields": "off",
-    "jsdoc/valid-types": "off"
+    'jsdoc/check-types': 'off',
+    'jsdoc/no-undefined-types': 'off',
+    'jsdoc/require-jsdoc': 'warn',
+    'jsdoc/require-param-description': 'off',
+    'jsdoc/require-property-description': 'off',
+    'jsdoc/require-returns-description': 'off',
+    'jsdoc/require-yields': 'off',
+    'jsdoc/valid-types': 'off'
   }
 }
